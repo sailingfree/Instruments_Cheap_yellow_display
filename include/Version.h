7d5b3ad912a3ab909f8a2de81d069f31923cfa09
sailingfree <peter@naiadhome.com>
2025-09-15 14:12:15 +0100

    Recompile

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.1"
+        #define VERSION "1.0.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-09-15 10:00:52.224082"
+        #define BUILD_TIMESTAMP "2025-09-15 10:10:22.880383"
     #endif
     
