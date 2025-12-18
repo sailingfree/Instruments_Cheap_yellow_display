4a77ea6f177f036c61acfe23ac46413d49538263
sailingfree <peter@naiadhome.com>
2025-12-18 10:20:44 +0000

    Move ds18b20 read to a separate task

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.83"
+        #define VERSION "1.0.87"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-12-17 22:01:42.886053"
+        #define BUILD_TIMESTAMP "2025-12-18 10:11:55.272705"
     #endif
     
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -11,24 +11,39 @@ OneWire oneWire(oneWireBus);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
+static float temperatureC;
+static float temperatureF;
+
+// Task to read the sensors
+void readSensors(void *parameter) {
+    static time_t last = 0;
+    while (1) {
+        time_t now = millis();
+
+        if (now - last > TEMPERATURE_PERIOD) {
+            last = now;
+            sensors.requestTemperatures();
+            temperatureC = sensors.getTempCByIndex(0);
+            temperatureF = sensors.getTempFByIndex(0);
+        }
+
+        // delay for 1000msecs
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void sensorsSetup() {
   // Start the DS18B20 sensor
   sensors.begin();
+
+  // Create a task to read the sensor in the background so we don't delay the main task
+  Serial.printf("Going to start sensors task\n");
+  xTaskCreate(readSensors, "readSensors", 8000, NULL, 1, NULL);
 }
 
-float temperatureC;
-float temperatureF;
 
 void sensorsWork() {
-    static time_t last = 0;
-    time_t now = millis();
 
-    if(now - last > TEMPERATURE_PERIOD) {
-        last = now;
-        sensors.requestTemperatures(); 
-        temperatureC = sensors.getTempCByIndex(0);
-        temperatureF = sensors.getTempFByIndex(0);
-    }
 }
 
 float getTempC() {
