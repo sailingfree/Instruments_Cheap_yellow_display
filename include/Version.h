5f6cd9d45c958d5d1d6ebe323715509575c04eb7
sailingfree <peter@naiadhome.com>
2026-01-01 10:31:27 +0000

    Remove unused directives

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.105"
+        #define VERSION "1.0.106"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-12-19 21:19:22.923143"
+        #define BUILD_TIMESTAMP "2026-01-01 10:31:09.490059"
     #endif
     
