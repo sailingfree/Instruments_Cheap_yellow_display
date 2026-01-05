a4b747857171950699527931781cd27d31808973
sailingfree <peter@naiadhome.com>
2026-01-05 10:50:39 +0000

    Add REST api for temperature and standard web page

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.106"
+        #define VERSION "1.0.113"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-01-01 10:31:09.490059"
+        #define BUILD_TIMESTAMP "2026-01-05 10:26:59.381459"
     #endif
     
--- src/MyWebServer.cpp
+++ src/MyWebServer.cpp
@@ -6,11 +6,15 @@
 
 #include <StringStream.h>
 #include <SysInfo.h>
+#include <sensors.h>
 
 // HTML strings
 #include <html/style.html>  // Must come before the content files
 #include <html/index.html>
 #include <html/login.html>
+
+// Json library
+#include <ArduinoJson.h>
 
 // The web server
 WebServer server(80);
@@ -30,6 +34,22 @@ void webServerSetup(void) {
             server.sendHeader("Connection", "close");
             });
 
+        server.on("/temp", HTTP_GET, []() {
+            float temp = getTempC();
+            server.sendHeader("Connection", "close");
+            server.send(200, "text/html", style +
+                head_html +
+                "<meta http-equiv='refresh' content='1'>" +
+                "<h1>System</h1>" +
+                "<h2>" +
+                "Temperature: " +
+                temp + 
+                "&#8451;" +
+                "</h2>" +
+                nav +
+                footer_html);
+        });
+
         server.on("/system", HTTP_GET, []() {
             StringStream net, sys, msgs;
             getNetInfo(net);
@@ -48,6 +68,27 @@ void webServerSetup(void) {
                 footer_html);
             });
 
+            server.on("/json", HTTP_GET, []() {
+                JsonDocument doc;
+                char * buffer;
+                size_t doclen;
+                float temp = getTempC();
+                doc["temperature"] = temp;
+                WiFiClient client = server.client();
+                // Write response headers
+                client.println(F("HTTP/1.0 200 OK"));
+                client.println(F("Content-Type: application/json"));
+                client.println(F("Connection: close"));
+                client.print(F("Content-Length: "));
+                client.println(measureJsonPretty(doc));
+                client.println();
+
+                // Write JSON document
+                serializeJsonPretty(doc, client);
+
+                // Disconnect
+                client.stop();
+            });
         delay(10);
         server.begin();
     }
